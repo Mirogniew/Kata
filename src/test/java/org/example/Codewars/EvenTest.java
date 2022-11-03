d3bc1f2360e0cd8ef3573459e69112e2e775ed79
Mirogniew <mirogniewny@gmail.com>
2022-11-03 18:29:39 +0100

    clean up

--- src/test/java/org/example/Codewars/EvenTest.java
+++ src/test/java/org/example/Codewars/EvenTest.java
@@ -6,7 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EvenTest {
     @Test
-    public void exampleTest() {
-        assertEquals("odd", Even.oddOrEven(new int[] {2, 5, 34, 6}));
+    public void exampleTest0() {
+        assertEquals("odd", Even.oddOrEven(new int[]{2, 5, 34, 6}));
+    }
+
+    @Test
+    public void exampleTest1() {
+        assertEquals("even", Even.oddOrEven(new int[]{1, 5, 35, 65}));
     }
 }
